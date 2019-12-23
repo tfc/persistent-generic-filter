148fbfbb64f85c126de894096d43f64e4f9a3250
Jacek Galowicz <jacek@galowicz.de>
2019-12-23 11:03:22 +0100

    Remove unneeded stuff

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
+import           Control.Monad           (liftM2)
 import           Control.Monad.IO.Class  (liftIO)
 import qualified Data.Map.Strict         as Map
 import           Data.Maybe              (catMaybes)
@@ -33,90 +34,10 @@ Person
 data FilterEntity record where
   FilterEntity :: (PersistField typ,
                    PersistEntity record,
-                   StringToVal record typ,
-                   ParseableFieldEntity record typ,
-                   ValidOperators record typ)
+                   ParseableFieldEntity record typ)
                => EntityField record typ -> FilterEntity record
 
 -------------------------------------------------------------------------------
-
--- TODO: toVal should return a Maybe SqlExpr because functions like read
--- could fail and then the whole expression makes no sense any longer.
-class StringToVal a b where
-    toVal :: EntityField a b -> String -> SqlExpr (Value b)
-
-instance StringToVal a Int where
-    toVal _ s = val (read s :: Int)
-
-instance StringToVal a String where
-    toVal _ = val
-
--------------------------------------------------------------------------------
-
-data FieldOperator = Equal
-                   | NotEqual
-                   | LEq
-                   | BEq
-                   | Lower
-                   | Bigger
-                   | Regex
-                   | Infix
-    deriving (Eq, Show)
-
-data Query = AND Query Query
-           | OR Query Query
-           | Predicate String FieldOperator String
-    deriving (Eq, Show)
-
-
-class ValidOperators a b where
-    validOps :: EntityField a b -> [FieldOperator]
-
-instance ValidOperators a Int where
-    validOps _ = [Equal, NotEqual, LEq, BEq, Lower, Bigger]
-
-instance ValidOperators a String where
-    validOps _ = [Equal, NotEqual, Regex, Infix]
-
-validOpsForEntity :: FilterEntity a -> [FieldOperator]
-validOpsForEntity (FilterEntity e) = validOps e
-
--------------------------------------------------------------------------------
-
-toQuery :: (FilterEntity a, String)
-        -> (SqlExpr (Entity a) -> SqlExpr (Value Bool))
-toQuery (FilterEntity e, s) p = p ^. (e) ==. toVal e s
-
-entityToString :: FilterEntity a -> FilterDict -> Maybe String
-entityToString (FilterEntity e) d = Map.lookup key d
-    where key = Text.unpack $ unHaskellName $ fieldHaskell
-              $ persistFieldDef e
-
-queryFromEntityFields :: PersistEntity a
-                      => [FilterEntity a]
-                      -> FilterDict
-                      -> (SqlExpr (Entity a) -> SqlExpr (Value Bool))
-queryFromEntityFields entities filters = let
-    entityToKeyValue e = (,) e <$> entityToString e filters
-    queries = map toQuery $ catMaybes $ map entityToKeyValue entities
-    in foldl1 (\l r x -> l x &&. r x) queries
-
-dbFieldParser :: Parser String
-dbFieldParser = spaces *> many1 letter
-
-dbOperatorParser :: Parser FieldOperator
-dbOperatorParser = spaces *> choice (map opParser ops)
-    where
-      opParser :: (String, FieldOperator) -> Parser FieldOperator
-      opParser (opString, opSymbol) = string opString *> pure opSymbol
-      ops = [
-              ("=", Equal)
-            , ("!=", NotEqual)
-            , ("<", Lower)
-            , (">", Bigger) -- TODO: Think about < being prefix of <= and adapt parser structure
-            , ("in", Infix)
-            , ("re", Regex)
-            ]
 
 type SqlPredicate a = SqlExpr (Entity a) -> SqlExpr (Value Bool)
 
@@ -146,12 +67,7 @@ parserForEntity (FilterEntity e) = do
     return (\p -> (p ^. e) `op` (val value))
 
 dbConjunctionOp :: Parser (SqlPredicate a -> SqlPredicate a -> SqlPredicate a)
-dbConjunctionOp = spaces *>
-    ((string "and" *> pure andF) <|> (string "or" *> pure orF))
-    where
-      andF l r p = l p &&. r p
-      orF  l r p = l p ||. r p
-
+dbConjunctionOp = spaces *> ( (string "and" *> pure (liftM2 (&&.))) <|> (string "or" *> pure (liftM2 (||.))))
 
 dbExprParser = dbFactorParser `chainl1` dbConjunctionOp
 dbFactorParser = spaces
